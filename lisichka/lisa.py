2353a345e71cace240a95cc85c2027815f206881
lalalisichka <dashink0306@gmail.com>
2025-12-04 09:58:26 +0300

    Update lisa.py

--- lisichka/lisa.py
+++ lisichka/lisa.py
@@ -1,34 +1,80 @@
 import telebot
 from config import API_TOKEN
+from telebot.types import (KeyboardButton, ReplyKeyboardMarkup,ReplyKeyboardRemove,Message)
 
-from telebot.types import (KeyboardButton,
-                    ReplyKeyboardMarkup,
-                    ReplyKeyboardRemove,
-                    Message)
-
-
-keyboard = ReplyKeyboardMarkup ()
-button = KeyboardButton (text="Моя кнопка")
-button2 = KeyboardButton(text="Точно моя кнопка")
-# button3 = KeyboardButton(text="Прям точно моя кнопка")
+API_TOKEN = '<api_token>'
+keyboard = ReplyKeyboardMarkup()
+button = KeyboardButton(text = 'Налево', request_location=True)
+button2 = KeyboardButton (text = 'Направо')
+# button3 = KeyboardButton (text = 'Моя кнопка 3')
 keyboard.add(button)
 keyboard.add(button2)
-keyboard.add("1","2")
-# keyboard.row("5","6","7")
-
+keyboard.add('1', '2')
 bot = telebot.TeleBot(API_TOKEN)
+state = tuple()
+state = 0
 
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
-def send_welcome(message:Message):
-    bot.reply_to(message, f"Привет!, {message.from_user.first_name}")
-
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+def send_welcome(message: Message):
+    global state
+    hello_message =  f'Привет, <b>{message.from_user.first_name}</b>!!!'
+    bot.send_message(
+        message.chat.id,
+        hello_message,
+        reply_markup=keyboard,
+          parse_mode= 'HTML'
+    )
+    state[message.from_user.id] = 1
+    print(state)
+    
 @bot.message_handler(func=lambda message: True)
-def echo_message(message: Message):
-    bot.reply_to(message, message.text)
+def text_message(message: Message):
+    global state
+    if state == 1 and message.text == 'Налево':
+        bot.send_message(
+            message.chat.id,
+            'Вы пошли налево во вторую комнату'
+        )
+        state = 2
+    elif state == 1 and message_text == 'Направо':
+        bot.send_message(
+            message.chat.id,
+            'Вы пошли направо в третью комнату'
+        )
+        state = 3
+elif message.text == "Налево" and state == 2:
+    bot.send_message(
+        message.from_user.id,
+        'Ты в комнате 4. Пути дальше нет.',
+  )
+    state = 0
 
+elif message.text == "Направо" and state == 2:
+    bot.send_message(
+        message.from_user.id,
+        'Ты в комнате 5. Пути дальше нет.',
+    )
+    state = 0
 
+elif message.text == "Налево" and state == 3:
+    bot.send_message(
+        message.from_user.id,
+        'Пути дальше нет.',
+    )
+    state = 0
+
+elif message.text == "Направо" and state == 3:
+    bot.send_message(
+        message.from_user.id,
+        'Пути дальше нет.',
+    )
+    state = 0
+
+else:
+    bot.send_message(
+        message.from_user.id,
+        'Не туда'
+    )
 bot.infinity_polling()
